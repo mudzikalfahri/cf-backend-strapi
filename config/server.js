ca7189cd24af4cf3e9c22ae1e22c0981439e24d5
unknown <alfahrimudzik@gmail.com>
2021-08-04 20:45:15 +0700

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ce09d3aeb334600ec27e7f712f046656'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '28a088e7-dfea-4b21-acf8-609c02ca89aa'
+};
